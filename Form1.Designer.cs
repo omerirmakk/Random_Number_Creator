449604b240d1dd8c2d9b036b7b806f266c901975
Omer Faruk IRMAK <omerfarukirmak@stu.ayvansaray.edu.tr>
2022-08-01 13:11:09 +0300

    Random ListNumber Generator

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,98 +31,119 @@ namespace GorselProgramlamaOdev
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(484, 69);
+            this.listBox1.Location = new System.Drawing.Point(27, 69);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(168, 225);
+            this.listBox1.Size = new System.Drawing.Size(111, 225);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(541, 317);
+            this.label1.Location = new System.Drawing.Point(69, 309);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(284, 69);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 49);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Soru 1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(284, 118);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 44);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Soru 2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(284, 162);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(133, 44);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Soru 3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(284, 206);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(133, 44);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Soru 4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(284, 250);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(133, 44);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "Soru 5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(84, 69);
+            this.listBox2.Location = new System.Drawing.Point(212, 69);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(120, 225);
+            this.listBox2.Size = new System.Drawing.Size(111, 225);
             this.listBox2.TabIndex = 7;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(253, 369);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 41);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Refresh";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(402, 69);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(111, 225);
+            this.listBox3.TabIndex = 9;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(596, 69);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(111, 225);
+            this.listBox4.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(787, 69);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(111, 225);
+            this.listBox5.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(827, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 41);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(725, 506);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(987, 506);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -137,12 +158,14 @@ namespace GorselProgramlamaOdev
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,24 +12,70 @@ namespace GorselProgramlamaOdev
 {
     public partial class Form1 : Form
     {
-                    
+        public bool AsalSayiBulma(int numm) // asal sayi kontrol methodu
+        {
+            if (numm == 1)
+            {
+                return false;
+            }
+            
+            for (int i = 2; i < numm ; i++)
+            {    
+                
+                if (numm % i == 0)
+                {
+                    return false;
+                }
+                
+            }
+                return true;
+        }
+
 
         public Form1()
         {
             InitializeComponent();
+           
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Random randomNumber = new Random();
+            
+            int maxnum=0;
+            double ort,top=0;
+
+            Random randomNumber = new Random(); // Soru 1 "Random sayı olusturma"
             int[] numbers = new int[25];
             for (int i = 0; i < numbers.Length; i++)
             {
-                
-                numbers[i] = randomNumber.Next(1,100);
-                listBox1.Items.Add(numbers[i].ToString());
+                numbers[i] = randomNumber.Next(1, 100);
+                listBox1.Items.Add(numbers[i].ToString()); // Soru 1 "random sayıları listbox'a yazdırma"
+                if (numbers[i] < 50) // Soru 2 "50'den kucuk sayıları alma"
+                {
+                    
+                    listBox2.Items.Add(numbers[i].ToString());
+                    label2.Text = listBox2.Items.Count.ToString(); // Soru 2 "50'den kucuk sayıları Label'a yazdırma"
+                }
+                if (numbers[i] > maxnum) // soru 4
+                {
+                    maxnum = numbers[i]; // Soru 4 Max number bulma
+                   
+                }
+                top += numbers[i];
+
+                if (AsalSayiBulma(numbers[i])) // soru 5
+                {
+                    listBox5.Items.Add(numbers[i]); // Asal sayi bulma (dizi ici)
+                    label3.Text = listBox5.Items.Count.ToString(); // Asal sayiyi Label'a yazdırma
+                }
             }
+            ort = top / numbers.Length;
+            listBox3.Items.Add(ort.ToString()); // Soru 3 "ortalama bulup listbox'a yazdırma
+            listBox4.Items.Add(maxnum.ToString()); // Soru 4 max number bulma ve yazdırma
+            label1.Text = listBox1.Items.Count.ToString(); // soru 1
            
+
 
 
         }
@@ -43,7 +89,7 @@ namespace GorselProgramlamaOdev
 
         private void label1_Click(object sender, EventArgs e)
         {
-            label1.Text = listBox1.Items.Count.ToString();  
+            
 
         }
 
@@ -52,6 +98,63 @@ namespace GorselProgramlamaOdev
             
         }
 
-       
+        private void button6_Click(object sender, EventArgs e) // veriyi yenileme butonu
+        {
+            RefreshDataViewers();      
+        }
+        public void RefreshDataViewers() // veriyi yenileme methodu
+        {
+            ClearAll();
+            int maxnum = 0;
+            double ort, sum = 0;
+            Random randomNumber = new Random();
+            int[] numbers = new int[25];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = randomNumber.Next(1, 100);
+                listBox1.Items.Add(numbers[i].ToString());
+                if (numbers[i] < 50)
+                {
+
+                    listBox2.Items.Add(numbers[i].ToString());
+                    label2.Text = listBox2.Items.Count.ToString();
+                }
+                if (numbers[i] > maxnum)
+                {
+                    maxnum = numbers[i];
+
+                }
+                sum += numbers[i];
+
+                if (AsalSayiBulma(numbers[i]))
+                {
+                    listBox5.Items.Add(numbers[i]);
+                    label3.Text = listBox5.Items.Count.ToString();
+                }
+            }
+            ort = sum / 25;
+            listBox3.Items.Add(ort.ToString());
+            listBox4.Items.Add(maxnum.ToString());
+            label1.Text = listBox1.Items.Count.ToString();
+        }
+        private void ClearAll() // form'u komple temizleme methodu
+        { 
+           
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            listBox5.Items.Clear();
+            label1.Text = "0";
+            label2.Text = "0";
+            label3.Text = "0";
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ClearAll(); // Her seyi temizleme butonu
+        }
     }
 }
